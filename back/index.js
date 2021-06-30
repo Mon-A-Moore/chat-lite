0aec41adde414ddab4cdadbce9b2a8a5be2c73cf
Mon A Moore <anton.kirienko.2000@mail.ru>
2021-06-30 16:47:08 +0300

    fix cors

--- back/index.js
+++ back/index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server, {
-  cors: '*',
-});
-app.use(cors());
-app.use(express.json());
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require ('socket.io') (http)
+const cors = require('cors')
 
-io.on('connection', (socket) => {
+app.use(cors);
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+	next();
+});
+
+io.on('connection', socket => {
   socket.on('message', ({ name, message }) => {
-    io.emit('message', { name, message });
-  });
-});
+    io.emit('message', { name, message })
+  })
+})
 
-server.listen(4000, () => {
-  console.log('Сервер on port 4000');
-});
+http.listen(4000, function() {
+  console.log('Сервер on port 4000')
+})
